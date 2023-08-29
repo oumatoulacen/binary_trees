9f6d09ec5aebb56bfd0da245b6d341b45dcdd895
oumatoulacen <oumatoulacen@gmail.com>
2023-08-29 14:57:23 +0100

    create node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,14 +11,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *new_node;
 
-	node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	if (!node)
+	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (!new_node)
 		return (NULL);
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-	return (node);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
